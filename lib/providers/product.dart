732ba998235fde8672df77bfb3fdff05d5a5a47b
adampra28 <adamprabowo00@gmail.com>
2020-06-03 06:17:16 +0700

    Adding the Token to all request

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -24,11 +24,11 @@ class Product with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> toggleFavoritesStatus() async {
+  Future<void> toggleFavoritesStatus(String token) async {
     final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
-    final url = 'https://flutter-update-3fc1d.firebaseio.com/products/$id.json';
+    final url = 'https://flutter-update-3fc1d.firebaseio.com/products/$id.json?auth=$token';
     try {
       final response = await http.patch(url,
           body: json.encode({
--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -100,7 +100,7 @@ class Products with ChangeNotifier {
   }
 
   Future<void> addProduct(Product product) async {
-    const url = 'https://flutter-update-3fc1d.firebaseio.com/products.json';
+    final url = 'https://flutter-update-3fc1d.firebaseio.com/products.json?auth=$authToken';
     try {
       final response = await http.post(
         url,
@@ -132,7 +132,7 @@ class Products with ChangeNotifier {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
       final url =
-          'https://flutter-update-3fc1d.firebaseio.com/products/$id.json';
+          'https://flutter-update-3fc1d.firebaseio.com/products/$id.json?auth=$authToken';
       await http.patch(url,
           body: json.encode({
             'title': newProduct.title,
@@ -148,7 +148,7 @@ class Products with ChangeNotifier {
   }
 
   Future<void> deleteProduct(String id) async {
-    final url = 'https://flutter-update-3fc1d.firebaseio.com/products/$id.json';
+    final url = 'https://flutter-update-3fc1d.firebaseio.com/products/$id.json?auth=$authToken';
     final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
     var existingProduct = _items[existingProductIndex];
     _items.removeAt(existingProductIndex);
