7dfb3e826125c6fe2cf4caae112148d1636f8eb2
adampra28 <adamprabowo00@gmail.com>
2020-06-03 11:09:26 +0700

    Adding Logout Functionally

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -72,4 +72,11 @@ class Auth with ChangeNotifier {
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
   }
+
+  void logout() {
+    _token = null;
+    _userId = null;
+    _token = null;
+    notifyListeners();
+  }
 }
