035795894b442bad1a87bcda3fc26453961abbfd
adampra28 <adamprabowo00@gmail.com>
2020-06-03 21:27:10 +0700

    Working with "Animation Container"

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -130,7 +130,7 @@ class _AuthCardState extends State<AuthCard>
   @override
   void dispose() {
     super.dispose();
-      _controller.dispose();
+    _controller.dispose();
   }
 
   void _showErrorDialog(String message) {
@@ -220,14 +220,15 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(animation: _heightAnimation, builder: (ctx, ch) => Container(
-        // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints:
-            BoxConstraints(minHeight: _heightAnimation.value.height),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // height: _heightAnimation.value.height,
+        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
-        child: ch), child: Form(
+        child: Form(
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
